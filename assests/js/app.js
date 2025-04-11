9a0f3905fbcd8f385f293600f041329eb7ff3d29
khalidDababat <khaliddababat07@gmail.com>
2025-04-11 22:21:40 +0300

    End OF The Creating Task

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,50 +1,63 @@
 const taskInput = document.getElementById("task-input");
 const addbtn = document.getElementById("add-btn");
+const listTAsk = [];
 
-
-
-
-
-function AddTask() { 
+function AddTask() {
   const text = taskInput.value.trim();
   if (text == "") {
     return;
-
   } else {
     createTask(text);
   }
 }
 
-function createTask(text){ 
+function createTask(text) {
+  const ullist = document.getElementById("conteaner-list");
+  const li = document.createElement("li");
+  const inputcheck = document.createElement("input");
+  const label = document.createElement("label");
+  const spanremove = document.createElement("span");
+  const spanedit = document.createElement("span");
 
-     
-    const ullist = document.getElementById('conteaner-list'); 
-    const li     =document.createElement('li'); 
-    const inputcheck  =document.createElement('input');
-    const label  =document.createElement('label'); 
-    const spanremove  =document.createElement('span');
-    const spanedit  =document.createElement('span');
+  const prority = document.getElementsByName("prority");
 
-    inputcheck.setAttribute("type","checkbox");
-    label.innerHTML =`${text}`; 
-    spanremove.setAttribute("class",'icon-remove'); 
-    spanedit.setAttribute('class','icon-edit'); 
-    spanremove.innerHTML ="&#128465"; 
-    spanedit.innerHTML = "&#9998;"; 
+  inputcheck.setAttribute("type", "checkbox");
+  label.innerHTML = `${text}`;
+  spanremove.setAttribute("class", "icon-remove");
+  spanedit.setAttribute("class", "icon-edit");
+  spanremove.innerHTML = "&#128465";
+  spanedit.innerHTML = "&#9998;";
 
-    ullist.appendChild(li); 
-    li.appendChild(inputcheck);
-    li.appendChild(label);
-    li.appendChild(spanremove);
-    li.appendChild(spanedit);
+  ullist.appendChild(li);
+  li.appendChild(inputcheck);
+  li.appendChild(label);
+  li.appendChild(spanremove);
+  li.appendChild(spanedit);
 
-    // const obj ={
-    //     "task":text,
-    //     "proprity":
-    // }; 
-     
-     
-    console.log("Done"); 
-} 
+  let prorityTask;
+
+  for (let i = 0; i < prority.length; i++) {
+    if (prority[i].checked == true) {
+      prorityTask = prority[i].value;
+      // console.log("The Prority Task ",  prorityTask);
+    }
+  }
+
+  const obj = {
+    task: text,
+    proprity: prorityTask,
+    completed: false,
+  };
+  listTAsk.push(obj);
+
+  
+
+}
+
+// const li = document.querySelector('li');
+// li.addEventListener("click",()=>{
+   
+// });
+
 
 
