9aad99b7418f102e451cf2150d86506dc0343850
khalidDababat <khaliddababat07@gmail.com>
2025-04-15 14:03:37 +0300

    feat:add function Add Task

---
+++ assests/js/app.js
@@ -0,0 +1,12 @@
+const taskInput = document.getElementById("task-input");
+const addbtn = document.getElementById("add-btn"); 
+
+
+function AddTask() {
+    const text = taskInput.value.trim();
+    if (text == "") {
+      return;
+    } else {
+    //   createTask(text);  
+    }
+  }
